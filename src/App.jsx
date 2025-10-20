4a0e05e62e9dc605f142d7c25c864116e385e8a7
nicoblan <blanconicolas139@gmail.com>
2025-10-20 02:31:03 -0300

    Carrito correcion

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,5 @@
-<<<<<<< Updated upstream
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import ShopPage from './views/ShopPage.jsx';
-import AdminPanel from './views/AdminPanel.jsx';
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
-import ProtectedRoute from './components/ProtectedRoute';
-import { AuthProvider } from './context/AuthContext';
-import { CartProvider } from './context/CartContext';
-
-function App() {
-  return (
-    <AuthProvider>
-      <CartProvider>
-        <Router>
-          <div className="min-h-screen flex flex-col">
-            <Navbar />
-            
-            <main className="flex-grow">
-              <Routes>
-                <Route path="/" element={<ShopPage />} />
-                <Route 
-                  path="/admin" 
-                  element={
-                    <ProtectedRoute adminOnly={true}>
-                      <AdminPanel />
-                    </ProtectedRoute>
-                  } 
-                />
-              </Routes>
-            </main>
-            
-            <Footer />
-          </div>
-        </Router>
-      </CartProvider>
-    </AuthProvider>
-=======
 import { Routes, Route } from 'react-router-dom';
-import ShopPage from './views/ShopPage.jsx'; 
+import ShopPage from './views/ShopPage.jsx';
 import Carrito from './views/Carrito.jsx';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
@@ -56,7 +18,6 @@ function App() {
       
       <Footer />
     </div>
->>>>>>> Stashed changes
   );
 }
 
