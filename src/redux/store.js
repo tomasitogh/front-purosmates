f8179421b0fb5ae234490f082251a3c6521f4e39
Tomas Gonzalez Humphreys <tomasgonzalezhu.phreys12@gmail.com>
2025-11-25 00:44:25 -0300

    Se quitó documentación y se quitó ejemplo de profesora de Slice para implementación de Redux

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,5 @@
 // Acá vamos a almacenar nuestro gran estado global y nos ahorra toda la parte de middleware
 import { configureStore } from "@reduxjs/toolkit";
-// importa el reducer de Post (no tenemos posts o publicaciones, es a modo de ejemplo)
-import postReducer from "./postSlice";
 import productReducer from "./productSlice";
 import authReducer from "./authSlice";
 import categoryReducer from "./categorySlice";
@@ -11,7 +9,6 @@ import fileReducer from "./fileSlice";
 
 export const store = configureStore({
     reducer: {
-        posts: postReducer,
         products: productReducer,
         auth: authReducer,
         categories: categoryReducer,
